59f01a4ed12855563d67fec07b4548eec2b160fe
nesnavarro <108523428+nesnavarro@users.noreply.github.com>
2023-07-13 17:51:01 +0200

    Update script-slider.js deleting comments

--- js/script-slider.js
+++ js/script-slider.js
@@ -5,12 +5,6 @@
     const nextBtn = document.querySelector(".next");
     let currentIndex = 0;
 
-    // function updateSliderContainerSize() {
-    //   const currentSlide = slider.children[currentIndex];
-    //   const slideHeight = currentSlide.offsetHeight;
-    //   sliderContainer.style.height = `${slideHeight}px`;
-    // }
-
     function prevSlide() {
       if (currentIndex > 0) {
         currentIndex--;
@@ -27,15 +21,10 @@
     }
 
     function slideToIndex(index) {
-      // const slideHeight = slider.children[index].offsetHeight;
       const slideWidth = slider.offsetWidth;
       slider.style.transform = `translateX(-${index * slideWidth}px)`;
-      // sliderContainer.style.height = `${slideHeight}px`;
     }
 
     prevBtn.addEventListener("click", prevSlide);
     nextBtn.addEventListener("click", nextSlide);
-
-    // window.addEventListener("resize", updateSliderContainerSize);
-    // updateSliderContainerSize();
   });
